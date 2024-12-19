cdcfda8d71f071a59782e578a2b3b96fecf2c7ed
Arkadeep Bandyopadhyay <arkadeep.b@gmail.com>
2024-12-19 10:26:03 +0530

    Update file 12/19/2024, 10:26:03 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,19 +1,8 @@
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if nums[i] + nums[j] == target:
-#                     return [i, j]
-
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in seen:
-                return [seen[diff], i]
-            else:
-                seen[nums[i]] = i
-
-        
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []  # No solution found
