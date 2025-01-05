e72f30b7a856a9d7adfabfaef1a63657e75f377a
Arkadeep Bandyopadhyay <arkadeep.b@gmail.com>
2025-01-05 00:37:06 -0600

    Create file

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        pointer1 = 0
+
+        pointer2 = len(s) - 1
+
+        # removing non alphanumeric character
+        s = (''.join(ch for ch in s if ch.isalnum()))
+
+        # converting to lowercase
+        s = s.lower()
+
+        if s == s[::-1]:
+            return True
+        else:
+            return False
