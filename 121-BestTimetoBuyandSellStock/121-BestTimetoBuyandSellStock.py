d2aef63b92a65a50c8bc9e6a878389e4f45fbee3
Arkadeep Bandyopadhyay <arkadeep.b@gmail.com>
2025-01-08 00:36:47 -0600

    Create file

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        p1 = 0
+        p2 = 1
+
+        max_profit = 0
+
+        while p1 < len(prices) and p2 < len(prices):
+            
+            # print(f"Current pointers: {p1}, {p2}")
+            profit = prices[p2] - prices[p1]
+            # print(f"Current profit: {profit}")
+
+            if profit < 0:
+                p1 += 1
+            else:
+                p2 += 1
+                max_profit = max(profit, max_profit)
+
+        return max_profit
