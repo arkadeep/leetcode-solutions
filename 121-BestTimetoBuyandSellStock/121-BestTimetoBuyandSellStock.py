d73936181c60fc90306802982842ff4adbef41a2
Arkadeep Bandyopadhyay <arkadeep.b@gmail.com>
2025-01-08 00:57:39 -0600

    Update file 1/8/2025, 12:57:38 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -13,9 +13,28 @@ class Solution:
             # print(f"Current profit: {profit}")
 
             if profit < 0:
-                p1 = p2
+                p1 += 1
             else:
                 p2 += 1
                 max_profit = max(profit, max_profit)
 
         return max_profit
+
+
+
+    # More efficient solution:
+
+
+    # class Solution:
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     l, r = 0, 1
+    #     maxP = 0
+
+    #     while r < len(prices):
+    #         if prices[l] < prices[r]:
+    #             profit = prices[r] - prices[l]
+    #             maxP = max(maxP, profit)
+    #         else:
+    #             l = r
+    #         r += 1
+    #     return maxP
